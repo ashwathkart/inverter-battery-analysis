7083c8dae3cb9890396601d46c3f591c523e593c
Ashwath Karthikeyan <86948494+ashwath-karthikeyan@users.noreply.github.com>
2021-09-15 17:34:00 +0530

    Update Inverter.sql

--- Inverter.sql
+++ Inverter.sql
@@ -3,19 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 15, 2021 at 05:32 PM
--- Server version: 10.3.29-MariaDB-0+deb10u1
--- PHP Version: 7.3.29-1~deb10u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `Inverter`
 --
